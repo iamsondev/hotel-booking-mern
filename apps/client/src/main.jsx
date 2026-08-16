95e23c129a4bff2429b403df9eed1e77511828b8
iamsondev <sondipkumarsk@gmail.com>
2026-08-16 13:56:52 +0600

    feat(client): integrate GoogleOAuthProvider in main.jsx

--- apps/client/src/main.jsx
+++ apps/client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { store } from './app/store';
 import './index.css';
 import App from './app/App.jsx';
@@ -10,10 +11,12 @@ import App from './app/App.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Toaster position="top-right" reverseOrder={false} />
-        <App />
-      </BrowserRouter>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ""}>
+        <BrowserRouter>
+          <Toaster position="top-right" reverseOrder={false} />
+          <App />
+        </BrowserRouter>
+      </GoogleOAuthProvider>
     </Provider>
   </StrictMode>
 );
